4c69d4a32660292a062c12f874e58c1443cb242e
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-10 09:32:17 -0300

    Update virt-manager.sh

--- virt-manager.sh
+++ virt-manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install desktop-file-utils debootstrap schroot perl git wget xz-utils bubblewrap autoconf coreutils -y
+sudo apt install desktop-file-utils debootstrap perl git wget xz-utils bubblewrap autoconf coreutils -y
 wget -q "https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagetool && chmod a+x appimagetool
 wget -c "https://archive.archlinux.org/iso/"
 wget -c "https://github.com/xplshn/pelf/releases/download/190/pelf_x86_64" -o pelf && chmod a+x pelf
@@ -10,8 +10,9 @@ tar xf archlinux-bootstrap-x86_64.tar.zst -C ./arch/
 # criar no github uma nova pasta parao AppRun e demais arquivos.
 cp ${GITHUB_WORKSPACE}/resolv.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/ && cp ${GITHUB_WORKSPACE}/mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/chaotic-mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S --noconfirm libpng libjpeg-turbo giflib libtiff librsvg webp libheif libavif gdk-pixbuf2 dnsmasq bridge-utils openbsd-netcat virt-manager-git swtpm dnsmasq virtiofsd qemu-full jack2 --noconfirm && pacman -Scc --noconfirm && rm -rf /var/cache/pacman/pkg/* && sed -i '17,$ {/^[#] *[a-zA-Z]/s/^# *//}' /etc/locale.gen && locale-gen"
-wget -c "https://github.com/lucasmz1/bubblewrap-musl-static/releases/download/7f9bc5f/bwrap-x86_64" && mv bwrap-x86_64 bwrap && chmod +x bwrap && mv bwrap ./arch/
+sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Sy --noconfirm && pacman -S --noconfirm libpng libjpeg-turbo giflib libtiff librsvg webp libheif libavif gdk-pixbuf2 dnsmasq bridge-utils openbsd-netcat virt-manager-git swtpm dnsmasq virtiofsd qemu-full jack2 --noconfirm && pacman -Scc --noconfirm && rm -rf /var/cache/pacman/pkg/* && sed -i '17,$ {/^[#] *[a-zA-Z]/s/^# *//}' /etc/locale.gen && locale-gen"
+wget -c "https://github.com/lucasmz1/bubblewrap-musl-static/releases/download/7f9bc5f/bwrap-x86_64"
+mv bwrap-x86_64 bwrap && chmod +x bwrap && mv bwrap ./arch/
 cp ${GITHUB_WORKSPACE}/AppRun ${GITHUB_WORKSPACE}/arch/
 chmod a+x ${GITHUB_WORKSPACE}/arch/AppRun 
 cp ${GITHUB_WORKSPACE}/virt-manager.desktop -t ${GITHUB_WORKSPACE}/arch/ 
