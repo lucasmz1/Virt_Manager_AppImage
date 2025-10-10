5133ee512635608eddbbdba21b930e99757d9770
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-10-10 08:44:32 -0300

    Update virt-manager.sh

--- virt-manager.sh
+++ virt-manager.sh
@@ -8,9 +8,9 @@ find ${GITHUB_WORKSPACE} -name '*.zst' | xargs -i -t -exec mv {} ${GITHUB_WORKSP
 mkdir arch
 tar xf archlinux-bootstrap-x86_64.tar.zst -C ./arch/
 # criar no github uma nova pasta parao AppRun e demais arquivos.
-cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/ && cp ${GITHUB_WORKSPACE}/mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/
+cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/ && cp ${GITHUB_WORKSPACE}/mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/chaotic-mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S --noconfirm libpng libjpeg-turbo giflib libtiff librsvg webp libheif libavif gdk-pixbuf2 dnsmasq bridge-utils openbsd-netcat virt-manager swtpm dnsmasq virtiofsd qemu-full jack2 --noconfirm && pacman -Scc --noconfirm && rm -rf /var/cache/pacman/pkg/* && sed -i '17,$ {/^[#] *[a-zA-Z]/s/^# *//}' /etc/locale.gen && locale-gen"
+sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S --noconfirm libpng libjpeg-turbo giflib libtiff librsvg webp libheif libavif gdk-pixbuf2 dnsmasq bridge-utils openbsd-netcat virt-manager-git swtpm dnsmasq virtiofsd qemu-full jack2 --noconfirm && pacman -Scc --noconfirm && rm -rf /var/cache/pacman/pkg/* && sed -i '17,$ {/^[#] *[a-zA-Z]/s/^# *//}' /etc/locale.gen && locale-gen"
 wget -c "https://github.com/lucasmz1/bubblewrap-musl-static/releases/download/7f9bc5f/bwrap-x86_64" && mv bwrap-x86_64 bwrap && chmod +x bwrap && mv bwrap ./arch/
 cp ${GITHUB_WORKSPACE}/AppRun ${GITHUB_WORKSPACE}/arch/
 chmod a+x ${GITHUB_WORKSPACE}/arch/AppRun 
